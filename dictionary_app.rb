a8713ca3145a7e95c7172218f6405081bb3f7d6e
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-04-29 20:02:44 +0200

    exercise with top example and pronounciation

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -4,11 +4,16 @@ system "clear"
 puts "Please input a word to find its meaning:"
 input_word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 word_data = response.parse(:json)
 
 definition = word_data[0]["text"]
-# top_example =
+top_example = word_data[0]["exampleUses"][0]["text"]
+pronounciation = word_data[0]["textProns"][0]
 
 puts "The definition is: 
-#{definition}"
+#{definition}.
+Here is an example of the word in a sentence:
+#{top_example}
+Here is how you pronounce it:
+#{pronounciation}"
