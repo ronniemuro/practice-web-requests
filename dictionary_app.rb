9d7ffb64ad0dbe108c2c3f7daefd9c75fb466174
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-04-29 22:59:15 +0200

    dictionary app with definition, top example, pronounciation, and loop

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,19 +1,46 @@
 require "HTTP"
 
-system "clear"
-puts "Please input a word to find its meaning:"
-input_word = gets.chomp
+while true
+  system "clear"
+  puts "Please input a word to find its meaning:"
+  input_word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
-word_data = response.parse(:json)
+  #DEFINITIONS
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+  definitions = response.parse(:json)
 
-definition = word_data[0]["text"]
-top_example = word_data[0]["exampleUses"][0]["text"]
-pronounciation = word_data[0]["textProns"][0]
+  puts "DEFINITIONS:"
+  i = 0
+  definitions.length.times do
+    definition = definitions[i]["text"]
+    puts "#{i + 1}. #{definition}."
+    i += 1
+  end
 
-puts "The definition is: 
-#{definition}.
-Here is an example of the word in a sentence:
-#{top_example}
-Here is how you pronounce it:
-#{pronounciation}"
+  #TOP_EXAMPLES
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+  top_examples = response.parse(:json)
+
+  puts
+  puts "TOP EXAMPLES:"
+  puts top_examples["text"]
+
+  puts
+
+  #PRONOUNCIATIONS
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/slay/pronunciations?useCanonical=false&limit=50&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+  pronounciations = response.parse(:json)
+
+  puts "PRONOUNCIATIONS"
+  i = 0
+  pronounciations.length.times do
+    pronounciation = pronounciations[i]["raw"]
+    puts "#{i + 1}. #{pronounciation}."
+    i += 1
+  end
+  puts "Press any key to input another word or q to quit program..."
+  if input_word == "q"
+    break
+    puts "Thanks! bye"
+  end
+end
